03a5c918fa04702eb26a1f659e7c6832478c6eeb
HuddlrChief <147014069+HuddlrChief@users.noreply.github.com>
2026-07-20 22:42:14 -0500

    Add event listeners to numbers and operators.

--- javascript.js
+++ javascript.js
@@ -15,8 +15,14 @@ function divide (a, b ) {
 }
 
 let operator = null
-let op1 = null
-let op2 = null
+let op1 = ""
+let op2 = ""
+
+const numberButtons = document.querySelectorAll(".button.number");
+const operatorButtons = document.querySelectorAll(".button.operator");
+const equalsSign = document.querySelector(".equals-sign");
+const clearButton = document.querySelector(".button.clear");
+const displayScreen = document.querySelector(".display-screen")
 
 function operate (action, firstOp, secondOp) {
     if (action === "+") {
@@ -33,3 +39,33 @@ function operate (action, firstOp, secondOp) {
     }
 }
 
+function changeNumber (number) {
+    if (!operator) {
+        op1 += number;
+        displayScreen.innerText = op1;
+    }
+    else {
+        op2 += number;
+        displayScreen.innerText = op2;
+    }
+    console.log(`op1 is ${op1}, op2 is ${op2}, 
+        operator is ${operator}`);
+}
+
+function changeOperator (newOp) {
+    operator = newOp;
+    console.log(`op1 is ${op1}, op2 is ${op2}, 
+        operator is ${operator}`);
+}
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    changeNumber(button.innerText);
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    changeOperator(button.innerText);
+  });
+});
